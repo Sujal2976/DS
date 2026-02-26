78e64d45cdef8d3c3af8994b71aeda6c600f1d97
Sujal2976 <sujalshah2976@gmail.com>
2026-02-26 09:48:49 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,20 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int a[10],n,i,pos,value;
-	printf("Enter number of elements");
+	int a[10],pos,value,i,n;
+	
+	printf("Enter Elemrnts in an array:\n");
 	for(i=0; i<8; i++)
 	{
-		scanf("%d" ,&a[10]);
+		scanf("%d",&a[i]);
 	}
-	printf("Enter position to insert element:");
-	scanf("%d" ,&pos);
+	printf("Enter position To insert element:");
+	scanf("%d",&pos);
 	
-	printf("Enter value to insert:");
-	scanf("%d" ,&value);
-	for(i=n; i<pos; i--)
+	printf("Enter value to insert");
+	scanf("%d",&value);
+	for(i=n,i>pos,i--)
 	{
 		a[i]=a[i-1];
 	}
-	a[pos-1]=value;
+	a[pos-i]=value;
+	printf("Array after indetion:")
+		for (int i=0;i<10;i++)
+		{
+			printf("%d",a[i]);
+			return 0;
+		}
 }
